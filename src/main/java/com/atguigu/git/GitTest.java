55bd5882cfb248f297c3345c81261aa75a07b236
MengLeon1 <18815426560@163.com>
2023-02-08 11:38:42 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git! hot-fix");
         System.out.println("hello git! hot-fix test");
         System.out.println("hello git! hot-fix test2");
+        System.out.println("push commit");
     }
 }
